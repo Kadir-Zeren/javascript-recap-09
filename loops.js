840ff172c9064bd086a9c066eda9bac018c6592b
kadir-zeren <k4dzeren@gmail.com>
2024-11-13 18:08:55 +0300

    for in

--- loops.js
+++ loops.js
@@ -16,18 +16,35 @@ console.log(" *** LOOPS IN ARRAYS *** ");
 
 // console.log("AVG:", (sum / grades.length).toFixed(2));
 
-//?-------------ÖRNEK---------------
-//? grades dizisindeki notların 50'den kucuk olanları ve buyuk olanları 2 ayrı diziye kaydediniz.
+// //?-------------ÖRNEK---------------
+// //? grades dizisindeki notların 50'den kucuk olanları ve buyuk olanları 2 ayrı diziye kaydediniz.
+// const grades = [55, 77, 23, 89, 100, 44, 33, 45];
+// const lessThan50 = [];
+// const equalOrBiggerThan50 = [];
+
+// for (let j = 0; j < grades.length; j++) {
+//   if (grades[j] < 50) {
+//     lessThan50.push(grades[j]);
+//   } else {
+//     equalOrBiggerThan50.push(grades[j]);
+//   }
+// }
+
+// console.log(lessThan50);
+// console.log(equalOrBiggerThan50);
+// console.log(grades);
+
+//? ----------------ORNEK--------------------
+//? grades dizisindeki notların 50'den kucuk olanları ve buyuk olanları 2 a diziye kaydediniz. FOR-IN
+
 const grades = [55, 77, 23, 89, 100, 44, 33, 45];
 const lessThan50 = [];
 const equalOrBiggerThan50 = [];
 
-for (let j = 0; j < grades.length; j++) {
-  if (grades[j] < 50) {
-    lessThan50.push(grades[j]);
-  } else {
-    equalOrBiggerThan50.push(grades[j]);
-  }
+for (let i in grades) {
+  grades[i] < 50
+    ? lessThan50.push(grades[i])
+    : equalOrBiggerThan50.push(grades[i]);
 }
 
 console.log(lessThan50);
